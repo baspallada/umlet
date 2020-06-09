fdbabcdbc1e863ccec33e88784185063fd17eed8
Bas Pallada <baspallada@hotmail.com>
2020-06-09 13:44:35 +0200

    Added unit test file

---
+++ umlet-standalone/src/test/java/nl/hva/asv/AsvTests.java
@@ -0,0 +1,12 @@
+package nl.hva.asv;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AsvTests {
+
+    @Test
+    public void testingTest() {
+        assertTrue(true);
+    }
+}
